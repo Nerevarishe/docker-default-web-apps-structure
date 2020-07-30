4207bd7ec3f69f552b018612864a63bd1dc45885
Link <Nerevarishe@users.noreply.github.com>
2020-07-30 10:52:22 +0300

    Update run-project.sh

--- run-project.sh
+++ run-project.sh
@@ -4,5 +4,6 @@ echo 'Building default images'
 docker build -f ./alpine-default-images/Dockerfile-clear-alpine-image -t nerevarishe/alpine:latest
 docker build -f ./alpine-default-images/Dockerfile-node-alpine-image -t nerevarishe/node-alpine:latest
 docker build -f ./alpine-default-images/Dockerfile-python-alpine-image -t nerevarishe/python-alpine:latest
+docker build -f ./alpine-default-images/Dockerfile-nginx-alpine-image -t nerevarishe/nginx-alpine:latest 
 
 echo 'Building dependencies images'
